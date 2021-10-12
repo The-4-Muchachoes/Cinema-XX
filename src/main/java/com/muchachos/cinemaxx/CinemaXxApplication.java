fe4914ba7274c6f9f445d1365905e296ddd4b68c
Lars <larsriel@gmail.com>
2021-10-12 15:01:30 +0200

    first commit

---
+++ src/main/java/com/muchachos/cinemaxx/CinemaXxApplication.java
@@ -0,0 +1,13 @@
+package com.muchachos.cinemaxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaXxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaXxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/muchachos/cinemaxx/CinemaXxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muchachos.cinemaxx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaXxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
