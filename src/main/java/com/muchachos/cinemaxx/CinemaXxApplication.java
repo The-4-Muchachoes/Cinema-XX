a1237480d746b033dc34ee8a283951457a162d78
Lars <larsriel@gmail.com>
2021-10-13 17:19:38 +0200

    test

--- src/main/java/com/muchachos/cinemaxx/CinemaXxApplication.java
+++ src/main/java/com/muchachos/cinemaxx/CinemaXxApplication.java
@@ -1,12 +1,22 @@
 package com.muchachos.cinemaxx;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class CinemaXxApplication {
 
+    static Logger logger = LoggerFactory.getLogger(CinemaXxApplication.class);
+
     public static void main(String[] args) {
+
+        Map<String, String> env = System.getenv();
+        env.forEach((k, v) -> logger.info(k + ":" + v));
+
         SpringApplication.run(CinemaXxApplication.class, args);
     }
 
