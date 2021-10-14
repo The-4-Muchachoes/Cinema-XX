68d21ca11a93af99c68a07c77a2b05bb1f48cbcf
Lars <larsriel@gmail.com>
2021-10-14 14:43:59 +0200

    fixed endpoint conflict

--- src/main/java/com/muchachos/cinemaxx/Screening/Controller/ScreeningController.java
+++ src/main/java/com/muchachos/cinemaxx/Screening/Controller/ScreeningController.java
@@ -4,10 +4,7 @@ import com.muchachos.cinemaxx.Screening.DTO.ScreeningDTO;
 import com.muchachos.cinemaxx.Screening.Service.ScreeningServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -19,21 +16,21 @@ public class ScreeningController {
     @Autowired
     ScreeningServiceImpl screeningService;
 
-    @GetMapping
-    private Iterable<ScreeningDTO> getScreeningsByCinemaAndDate(@RequestParam int cinemaId, @RequestParam String date) {
+    @GetMapping(path = "/{cinema_id}")
+    private Iterable<ScreeningDTO> getScreeningsByCinemaAndDate(@PathVariable int cinema_id, @RequestParam String date) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         LocalDate d = LocalDate.parse(date, formatter);
-        return screeningService.getTitleTimeAndRatingByCinemaAndDate(cinemaId, d, null);
+        return screeningService.getTitleTimeAndRatingByCinemaAndDate(cinema_id, d, null);
     }
 
-    @GetMapping
-    private Iterable<ScreeningDTO> getScreeningsByCinemaBetweenDates(@RequestParam int cinemaId,
+    @GetMapping(path = "/{cinema_id}/between_dates")
+    private Iterable<ScreeningDTO> getScreeningsByCinemaBetweenDates(@PathVariable int cinema_id,
                                                                      @RequestParam String startDate,
                                                                      @RequestParam String endDate){
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         LocalDate d1 = LocalDate.parse(startDate, formatter);
         LocalDate d2 = LocalDate.parse(endDate, formatter);
-        return screeningService.getTitleTimeAndRatingByCinemaAndDate(cinemaId,d1,d2);
+        return screeningService.getTitleTimeAndRatingByCinemaAndDate(cinema_id,d1,d2);
 
     }
 }
