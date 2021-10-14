80c54149cf40a0384ca36d5c49a2484dcacac2bf
Lars <larsriel@gmail.com>
2021-10-14 18:34:45 +0200

    fixed a bug related to swagger

--- src/main/java/com/muchachos/cinemaxx/CinemaXxApplication.java
+++ src/main/java/com/muchachos/cinemaxx/CinemaXxApplication.java
@@ -1,23 +1,12 @@
 package com.muchachos.cinemaxx;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Map;
 
 @SpringBootApplication
 public class CinemaXxApplication {
 
-    static Logger logger = LoggerFactory.getLogger(CinemaXxApplication.class);
-
     public static void main(String[] args) {
-        System.out.println("SEARCHFORME");
-
-        Map<String, String> env = System.getenv();
-        env.forEach((k, v) -> System.out.println(k + ":" + v));
-
         SpringApplication.run(CinemaXxApplication.class, args);
     }
 
