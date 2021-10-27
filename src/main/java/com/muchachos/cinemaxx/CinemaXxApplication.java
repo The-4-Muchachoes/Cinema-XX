8e457233914a66ca55a1103c2c982619e549e4a2
Lars <larsriel@gmail.com>
2021-10-27 10:47:49 +0200

    commit to demonstrate Jenkins building automatically

--- src/main/java/com/muchachos/cinemaxx/CinemaXxApplication.java
+++ src/main/java/com/muchachos/cinemaxx/CinemaXxApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CinemaXxApplication {
 
+    // fake comment ...
     public static void main(String[] args) {
         SpringApplication.run(CinemaXxApplication.class, args);
     }
